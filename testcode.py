bf1b367160b8913fc09015268d7f9a04398b6a26
jimzwli <zhanwen.li@gmail.com>
2023-06-07 15:35:30 +1000

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -2,3 +2,14 @@ if a in x:
    x[a] += 1
 else:
    x[a] = 1
+
+   
+aa = {}
+for app_meta in rr:
+   id = app_meta[x']
+   an = app_meta['id']
+   if id in aa.keys():
+      ua = aa[staffid]
+      ua.append(an)
+   else:
+      aa[id] = [an]
