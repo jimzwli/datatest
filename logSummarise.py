c6cafed4dcf031eb371bf42598a0ac26ca6c2faf
jimzwli <zhanwen.li@gmail.com>
2023-06-08 12:06:33 +1000

    Update logSummarise.py

--- logSummarise.py
+++ logSummarise.py
@@ -6,63 +6,47 @@ class LogSummarise:
 
         red_count = 0
         yellow_count = 0
-        env_prod = 'Compliant'
-        env_non = 'Compliant'
-        is_env_prod_red = False
-        is_env_non_red = False
-        is_env_prod_amber = False
-        is_env_non_amber = False
-        issue_worst_case = {}
+        resource_a = 'Health'
+        resource_b = 'Health'
+        is_resource_a_red = False
+        is_resource_b_red = False
+        is_resource_a_amber = False
+        is_resource_b_amber = False
+    
         for issue in issues:
             issue_att = issue.get_attributes()
             issue_name = issue_att['issue_name']
-            issue_env = issue_att['issue_env']
-            score_value = issue_att['score_value']
+            issue_resource= issue_att['issue_resource']
+            state_value = issue_att['state_value']
             time_to_red = issue_att['time_to_red']
-            if score_value == 'r':
+            if state_value == 'r':
                 red_count += 1
-            if score_value == 'a':
+            if state_value == 'a':
                 yellow_count += 1
-            if issue_att['issue_env'].upper() == 'Z1' and score_value == 'r':
-                env_non = 'Non Compliant'
-                is_env_non_red = True
-            if issue_att['issue_env'].upper() == 'Z1' and score_value == 'a' and env_non == 'Compliant':
-                env_non = 'Due Soon'    
-            if issue_att['issue_env'].upper() == 'A1' and score_value == 'r':
-                env_prod = 'Non Compliant'
-                is_env_prod_red = True
-            if issue_att['issue_env'].upper() == 'A1' and score_value == 'a' and env_non == 'Compliant':
-                env_prod = 'Due Soon'                 
+            if issue_att['issue_resource'].upper() == 'Z1' and state_value == 'r':
+                resource_b = 'Non Health'
+                is_resource_b_red = True
+            if issue_att['issue_resource'].upper() == 'Z1' and state_value == 'a' and resource_b == 'Health':
+                resource_b = 'To Down'    
+            if issue_att['issue_resource'].upper() == 'A1' and state_value == 'r':
+                resource_a = 'Non Health'
+                is_resource_a_red = True
+            if issue_att['issue_resource'].upper() == 'A1' and state_value == 'a' and resource_b == 'Health':
+                resource_a = 'To Down'                 
 
-            if issue_att['issue_env'].upper() == 'Z1' and score_value == 'a':
-                is_env_non_amber = True 
-            if issue_att['issue_env'].upper() == 'A1' and score_value == 'a':
-                is_env_prod_amber = True 
-
-
-
-            if time_to_red is not None:
-                if issue_name in issue_worst_case:
-                    previous_time_to_red = issue_worst_case[issue_name] 
-                    print(f'previous time to red for {issue_name} in {system_name} is {previous_time_to_red}')
-                    if time_to_red > previous_time_to_red:
-                        issue_worst_case[issue_name] = time_to_red
-                    else:
-                        continue
-                else:
-                    issue_worst_case[issue_name] = time_to_red
-            if issue_name not in issue_worst_case:
-                issue_worst_case[issue_name] = -9999            
+            if issue_att['issue_resource'].upper() == 'Z1' and state_value == 'a':
+                is_resource_b_amber = True 
+            if issue_att['issue_resource'].upper() == 'A1' and state_value == 'a':
+                is_resource_a_amber = True        
 
         self.red = red_count
         self.yellow = yellow_count 
-        self.env_prod = env_prod
-        self.env_non = env_non
-        self.issue_worst_case = issue_worst_case  
-        self.is_env_prod_red = is_env_prod_red
-        self.is_env_non_red = is_env_non_red
-        self.is_env_non_amber = is_env_non_amber
-        self.is_env_prod_amber = is_env_prod_amber      
+        self.resource_a = resource_a
+        self.resource_b = resource_b 
+        self.is_resource_a_red = is_resource_a_red
+        self.is_resource_b_red = is_resource_b_red
+        self.is_resource_b_amber = is_resource_b_amber
+        self.is_resource_a_amber = is_resource_a_amber      
 
     def get_attributes(self):
         return self.__dict__
